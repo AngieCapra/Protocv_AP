672ae8b46f71cec8db89f296016bf06ce8716870
Angie Caprarulo <angie_caprarulo@hotmail.com>
2023-09-09 20:38:48 -0300

    Add files via upload
    Corrección de errores de sintaxis.

---
+++ cv_AP4.0/Function.js
@@ -0,0 +1,18 @@
+//abrir el modal
+function abrirModal() {
+  let modal = document.getElementById("modal");
+  modal.style.display = "block";
+}
+
+//cerrar el modal
+function cerrarModal() {
+  let modal = document.getElementById("modal");
+  modal.style.display = "none";
+}
+
+//evento clic para abrir el modal
+let enlace = document.getElementById("contact_form");
+enlace.addEventListener("click", function (event) {
+  event.preventDefault();
+  abrirModal();
+});
